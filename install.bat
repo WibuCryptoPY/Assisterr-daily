b35f26bf3456fd946843593a0fbfbc973cf05f3f
wibucrypto <56129456+wibucrypto2201@users.noreply.github.com>
2024-08-11 19:22:20 +0700

    Create install.bat

---
+++ install.bat
@@ -0,0 +1,9 @@
+pip install asyncio
+pip install random
+pip install hashlib
+pip install uuid
+pip install getpass
+pip install requests
+pip install loguru import logger
+pip install web3
+pip install colorama
